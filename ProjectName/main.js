4ff49cb113282b9a1c2c6ace546411e990d14701
MingHui <103197921+MingHui1997@users.noreply.github.com>
2022-07-30 11:11:59 +0800

    Add files via upload

---
+++ ProjectName/main.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+function HTML5main()
+{
+    var ResList = [];
+    
+    CT_init(960,540,ResList,CT_main,CT_loop,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0);
+}
+
+function CT_main()
+{
+    draw_string(400,200,24,"#00ff00","Hello World");
+}
+
+function CT_loop()
+{
+
+}
